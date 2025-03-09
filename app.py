d74f70320c64dc973a2c6ec8c3e96e0090048597
JTee9 <jimmytalbot9@gmail.com>
2025-03-09 21:46:04 +0900

    Initial Commit

--- app.py
+++ app.py
@@ -2,8 +2,9 @@ from dash import Dash, html
 import dash_bootstrap_components as dbc
 
 app = Dash(__name__, use_pages=True, external_stylesheets=[dbc.themes.SPACELAB], title='Data Scout FM')
+server = app.server
 
-layout = html.Div([html.H1('Hello World!')])
+app.layout = html.Div([html.H1('Hello World!')])
 
 if __name__ == '__main__':
     app.run(debug=True, port=8050)
